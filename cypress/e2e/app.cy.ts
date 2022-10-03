a0dc7aff018c5db6228eae5ddb2756300835c027
Michael Yuen <iam@mikey.baby>
2022-10-02 23:26:11 -0400

    Add cypress

---
+++ cypress/e2e/app.cy.ts
@@ -0,0 +1,8 @@
+describe("Search 'user:atom'", () => {
+  it("should navigate to the about page", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('input[type*="text"]').click().type("atom{enter}");
+    cy.get("h3").contains("atom/atom");
+    cy.url().should("include", "?q=atom");
+  });
+});
