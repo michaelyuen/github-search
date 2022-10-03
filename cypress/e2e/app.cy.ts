190dcd5be044fc88cde63d592238299181769648
Michael Yuen <iam@mikey.baby>
2022-10-03 13:05:51 -0400

    More cypress

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,9 +1,10 @@
-describe("Search 'user:atom'", () => {
-  it("should navigate to the about page", () => {
+export {};
+describe("empty spec", () => {
+  it("passes", () => {
+    // cy.visit('https://example.cypress.io')
     cy.visit("http://localhost:3000/");
     cy.get('input[type*="text"]').click().type("atom{enter}");
     cy.get("h3").contains("atom/atom");
     cy.url().should("include", "?q=atom");
   });
 });
-export {};
